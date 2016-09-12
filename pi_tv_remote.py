43ecae0adbfd91a8f53ac6c191de002559a84da4
Scott Thomson <scotty3785@gmail.com>
2016-09-12 20:12:08 +0100

    Add first test version

---
+++ pi_tv_remote.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from os import curdir
+import subprocess
+
+# Simple command
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def main():
+    templateData = {}
+    return render_template('remote.html', **templateData)
+
+@app.route('/send/<button>')
+def send_ir_command(button):
+    command = ['irsend','SEND_ONCE']
+    if button == "KEY_POWER":
+        command.append('Samsumg2')
+    else:
+        command.append('Samsung1')
+    command.append(button)
+    templateData = {'button':button}
+    subprocess.call(command, shell=True)
+    return "OK"
+    
+
+
+if __name__ == "__main__":
+    print(curdir)
+    app.run(host='0.0.0.0', port=81, debug=True)
